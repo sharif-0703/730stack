b6ad173ed593adf7b49bf0ef9d293fa7649f5da2
SAYED SHARIFUNNISA <sharifunnisasayed502@gmail.com>
2025-04-16 13:05:24 +0530

    update

---
+++ 730/JS/call stack hoisting/script.js
@@ -0,0 +1,12 @@
+console.log(a);
+console.log(b);
+var a = 6;
+var b = 3;
+console.log(a);
+console.log(b);
+function display() {
+  console.log(n);
+   var n = 10;
+  console.log(n);
+}
+display()
---
+++ 730/JS/primitive data types/script.js
@@ -0,0 +1,29 @@
+let x=25;
+console.log(x, typeof(x))
+let y=33.3566;
+console.log(y, typeof(y))
+console.log(Math.max(2,3))
+console.log(Math.min(2,3))
+console.log(Math.sqrt(4))
+console.log(Math.cbrt(16))
+console.log(y.toFixed(3))
+console.log(y.toPrecision(3))
+console.log(y.toString(2))
+console.log(Math.floor(2.5678))
+console.log(Math.ceil(1.5))
+console.log(Math.round(Math.random()*10000))
+
+let b= "sharif";
+console.log(b, typeof(b),b[0],b[1])
+
+let str= " Javascript is so easy "
+console.log(str.length)
+console.log(str.trim())
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+console.log(str.replace('a','b'))
+console.log(str.charAt(5) ,str.indexOf('a'), str.lastIndexOf('a'))
+console.log(str.startsWith("java"))
+console.log(str.endsWith("hi"))
+console.log(str.slice(2,5))
+console.log(str.split(' '))
